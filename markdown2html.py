a794f53f2800a16dd0d2c390f2b145c3c007b164
Opoku-Wiafe <opokuwiafevan@gmail.com>
2024-06-23 16:35:58 -0700

    fix first code

--- markdown2html.py
+++ markdown2html.py
@@ -1,52 +1,44 @@
 #!/usr/bin/python3
 """
-A script that convert a Markdown file to HTML.
-
+A script markdown that convert script to HTML
 Usage:
     ./markdown2html.py [markdown_file] [output_file]
-
-Arguments:
-    markdown_file: the name of the Markdown file to be converted
-    output_file: the name of the output HTML file
+markdown_file: Name of the file to be converted.
+output_file: The Name of the converted html file.
 """
 
-import argparse
-import pathlib
-import re
 import sys
+import os
+import markdown
 
 
-def convert_markdown_to_html(markdown_file, output_file):
+def main():
     """
-    Converts markdown file to HTML file
+    a script markdown2html.py that takes an argument 2 strings
     """
-    with open(markdown_file, encoding='utf-8') as f:
-        markd_content = f.readlines()
+    error_msg = "Usage: ./markdown2html.py README.md README.html"
 
-    html_content = []
-    for line in markd_content:
-        match = re.match(r'(#){1,6} (.*)', line)
-        if match:
-            h_level = len(match.group(1))
-            h_content = match.group(2)
-            html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
-        else:
-            html_content.append(line)
-
-    with open(output_file, 'w', encoding='utf-8') as f:
-        f.writelines(html_content)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert markdown to HTML')
-    parser.add_argument('input_file', help='path to input markdown file')
-    parser.add_argument('output_file', help='path to output HTML file')
-    args = parser.parse_args()
-
-    input_path = pathlib.Path(args.input_file)
-    if not input_path.is_file():
-        print(f'Missing {input_path}', file=sys.stderr)
+    if len(sys.argv) != 3:
+        print(error_msg, file=sys.stderr)
         sys.exit(1)
 
-    # Convert the markdown file to HTML
-    convert_markdown_to_html(args.input_file, args.output_file)
+    markdown_fname = sys.argv[1]
+    html_fname = sys.argv[2]
+
+    if not os.path.exists(markdown_fname):
+        print(f"Missing {markdown_fname}", file=sys.stderr)
+        sys.exit(1)
+
+    with open(markdown_fname, 'r') as md_file:
+        md_content = md_file.read()
+
+    html_content = markdown.markdown(md_content)
+
+    with open(html_fname, 'w') as html_file:
+        html_file.write(html_content)
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
